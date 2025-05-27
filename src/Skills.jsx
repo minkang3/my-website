6ae31ca197e5b43067fb9bbf39b38af7edd1ded1
Min Kang <86979005+minkang3@users.noreply.github.com>
2025-05-26 20:35:27 -0700

    Update skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -4,18 +4,16 @@ function Skills() {
       <p 
        className="text-right leading-[1.2] text-[1.3rem]"
       >
-        <strong>Fluent languages<br/> (I'm using them constantly)</strong><br/>
-        Embedded C, C++, Python, Javascript
+        <strong>Fluent languages<br/></strong><br/>
+        C,/C++,
         <br/><br/>
-        <strong>Strong languages<br/> (might need to search up documentation)</strong><br/>
-        Java, SQL
+        <strong>Strong languages<br/></strong><br/>
+        Python, Javascript, Java, SQL
         <br/><br/>
 
         <strong>Web Frameworks/Tools</strong><br/>
         React, Docker, PostgreSQL, TailwindCSS, Express
         <br/><br/>
-        All of these are things that I <em>know</em>.<br/>
-        This page isn't bloated with things I've dabbled with once.
       </p>
     </div>
   );
